1c9ff675893b548e730ca1b38d29f52ac3be4e3e
pyfong1579 <pyfong1@gmail.com>
2020-11-15 21:56:37 +0800

    Changed to overwritten data frame "data" for all selections requiring less memory

--- best.R
+++ best.R
@@ -10,20 +10,20 @@ best <- function(state, outcome){
   #Select variables of interest'
   outcomes <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
   
-  sel_data <- data[data$State == state, c(2,7,outcomes[[outcome]])]
+  data <- data[data$State == state, c(2,7,outcomes[[outcome]])]
   
   # rename columns
-  colnames(sel_data)<- c("hospital", "state", "outcome")
+  colnames(data)<- c("hospital", "state", "outcome")
   
   # remove NA
-  sel_data <- sel_data[complete.cases(sel_data),]
+  data <- data[complete.cases(data),]
   
   #select best hospitals with lowest outcome
-  best <- min(sel_data$outcome)
-  sel_data <- sel_data[sel_data$outcome == best,]
+  best <- min(data$outcome)
+  data <- data[data$outcome == best,]
   
   #sort alphabetical hospital name if tie
-  sort(sel_data$hospital)[1]
+  sort(data$hospital)[1]
 
   ## Return hospital name in that state with lowest 30-day death rate
 
